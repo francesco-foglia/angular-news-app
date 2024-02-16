27bc5fbaf23dbd1e56d3b729adceeae398947e69
Francesco Foglia <francescofogliajj@gmail.com>
2024-02-16 14:04:18 +0100

    Add unit tests for NewsComponent and NewsService

--- src/app/modules/news/news.component.spec.ts
+++ src/app/modules/news/news.component.spec.ts
@@ -1,23 +1,81 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NewsComponent } from './news.component';
+import { NewsService } from '../../services/api.service';
 
 describe('NewsComponent', () => {
   let component: NewsComponent;
   let fixture: ComponentFixture<NewsComponent>;
+  let newsService: NewsService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [NewsComponent]
-    })
-      .compileComponents();
+      declarations: [NewsComponent],
+      imports: [HttpClientTestingModule],
+      providers: [NewsService]
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(NewsComponent);
     component = fixture.componentInstance;
+    newsService = TestBed.inject(NewsService);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should fetch news items', async () => {
+    spyOn(newsService, 'getNewsIds').and.returnValue(Promise.resolve([1, 2, 3]));
+    spyOn(newsService, 'getNewsItem').and.callFake(async (id: number) => {
+      return { id, title: `News ${id}`, kids: [101, 102] };
+    });
+    spyOn(component, 'getComments').and.returnValue(Promise.resolve(['Comment 1', 'Comment 2']));
+
+    await component.fetchNews();
+
+    expect(component.newsItems.length).toBe(3);
+    expect(component.startIndex).toBe(component.batchSize);
+    expect(component.loading).toBe(false);
   });
+
+  it('should handle error when fetching news item', async () => {
+    spyOn(newsService, 'getNewsIds').and.returnValue(Promise.resolve([1, 2, 3]));
+    spyOn(newsService, 'getNewsItem').and.returnValue(Promise.reject('Fake error'));
+
+    await component.fetchNews();
+
+    expect(component.newsItems.length).toBe(0);
+    expect(component.loading).toBe(false);
+  });
+
+  it('should handle error when fetching news', async () => {
+    spyOn(newsService, 'getNewsIds').and.returnValue(Promise.reject('Fake error'));
+
+    await component.fetchNews();
+
+    expect(component.newsItems.length).toBe(0);
+    expect(component.loading).toBe(false);
+  });
+
+  it('should fetch comments for news items', async () => {
+    const mockNewsIds = [1, 2, 3];
+    spyOn(newsService, 'getNewsIds').and.returnValue(Promise.resolve(mockNewsIds));
+    spyOn(newsService, 'getNewsItem').and.callFake(async (id: number) => {
+      return { id, title: `News ${id}`, kids: [101, 102] };
+    });
+    spyOn(component, 'getComments').and.callThrough();
+
+    await component.fetchNews();
+
+    expect(component.getComments).toHaveBeenCalled();
+    expect(component.getComments).toHaveBeenCalledWith([101, 102]);
+  });
+
+  it('should load more news items when loadMore is called', async () => {
+    const fetchNewsSpy = spyOn(component, 'fetchNews').and.callThrough();
+
+    component.loadMore();
+
+    expect(fetchNewsSpy).toHaveBeenCalled();
+  });
+
 });
--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,16 +1,42 @@
 import { TestBed } from '@angular/core/testing';
-
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 import { NewsService } from './api.service';
 
 describe('NewsService', () => {
   let service: NewsService;
+  let mockAxios: any;
+  const baseUrl = 'https://hacker-news.firebaseio.com/v0/';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [NewsService]
+    });
     service = TestBed.inject(NewsService);
+    mockAxios = new MockAdapter(axios);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    mockAxios.reset();
   });
+
+  it('should retrieve news ids', async () => {
+    const mockIds = [1, 2, 3];
+    mockAxios.onGet(`${baseUrl}newstories.json`).reply(200, mockIds);
+
+    const newsIds = await service.getNewsIds();
+
+    expect(newsIds).toEqual(mockIds);
+  });
+
+  it('should retrieve news item', async () => {
+    const mockNewsItem = { id: 1, title: 'Mock News Item' };
+    const mockId = 1;
+    mockAxios.onGet(`${baseUrl}item/${mockId}.json`).reply(200, mockNewsItem);
+
+    const newsItem = await service.getNewsItem(mockId);
+
+    expect(newsItem).toEqual(mockNewsItem);
+  });
+
 });
