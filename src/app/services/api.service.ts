943620dec57d2a5c1540c49ee9ad74a51db96ab0
Francesco Foglia <francescofogliajj@gmail.com>
2024-02-16 14:04:17 +0100

    Add axios library and functions to fetch news ids and news items from the Hacker News API

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
+  private baseUrl = 'https://hacker-news.firebaseio.com/v0/';
 
   constructor() { }
+
+  async getNewsIds(): Promise<number[]> {
+    const response = await axios.get<number[]>(`${this.baseUrl}newstories.json`);
+    return response.data;
+  }
+
+  async getNewsItem(id: number): Promise<any> {
+    const response = await axios.get<any>(`${this.baseUrl}item/${id}.json`);
+    return response.data;
+  }
 }
